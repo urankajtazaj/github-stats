eb625b364f0201b83cdbbdf220755e9662c6b825
uran <ukajtazi@gmail.com>
2017-07-12 16:13:50 +0200

    don't clean inputs after submiting

--- index.php
+++ index.php
@@ -4,6 +4,14 @@
     if (isset($_GET['user']) && isset($_GET['repository'])) {
         if ($_GET['user'] != '' && $_GET['repository'] != '') {
             include 'functions/get-data.php';
+            ?>
+            
+            <script>
+                document.getElementById("name").value = "<?= $_GET['user'];?>";
+                document.getElementById("repo").value = "<?= $_GET['repository']; ?>";
+            </script>
+
+            <?php
         }
     }
     require 'includes/footer.php'; 
