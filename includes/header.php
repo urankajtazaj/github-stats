252c8f630bf5e6c55748790565536e7dde796d05
uran <ukajtazi@gmail.com>
2017-07-24 19:41:35 +0200

    style change

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title>Github release stats</title>
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="style/main.css"/>
     </head>
     <body>
